a983a33c8d81a878ea67fda821231da046496ecb
超人不会飞 <1429222731@qq.com>
2023-07-10 22:50:12 +0800

    项目初始化提交！

---
+++ mydb/src/test/java/com/charls/mydb/MydbApplicationTests.java
@@ -0,0 +1,15 @@
+package com.charls.mydb;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MydbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("起来啦!");
+    }
+
+}
